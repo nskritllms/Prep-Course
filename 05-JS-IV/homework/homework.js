dbe5f119d3554954fc6880db35f06b447269d05d
Marco A <marcodavilacastro1@gmail.com>
2022-04-13 12:38:28 -0500

    finalizado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -139,6 +139,12 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+  
+  for(var x = 0;x <usuario["posts"].length;x++){
+  suma = suma + usuario["posts"][x]["likes"];
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -151,7 +157,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+producto["calcularPrecioDescuento"] = function (){
+ return this.precio - (this.precio * this.porcentajeDeDescuento);
+}
+return producto;
 }
 
 // No modificar nada debajo de esta línea
